cee8a6ddb5152c73c3efd98471bb0dbf8b6c101c
Russell Gordon <rgordon@lcs.on.ca>
2024-01-27 09:16:47 -0500

    Finished adding a gallery in the form of a scrollable grid to our new landing view.

--- Landmarks/Views/GalleryView.swift
+++ Landmarks/Views/GalleryView.swift
@@ -8,23 +8,33 @@
 import SwiftUI
 
 struct GalleryView: View {
+    
+    // MARK: Stored properties
+    let twoColumns = [
+        GridItem(.adaptive(minimum: 100, maximum: 200), alignment: .top),
+        GridItem(.adaptive(minimum: 100, maximum: 200), alignment: .top),
+    ]
         
+    // MARK: Computed properties
     var body: some View {
         
         NavigationStack {
             
-            LazyVGrid {
+            ScrollView {
+                LazyVGrid(columns: twoColumns) {
 
-                ForEach(allLandmarks) { currentLandmark in
-                    
-                    NavigationLink {
-                        DetailView(item: currentLandmark)
-                    } label: {
-                        GalleryItemView(item: currentLandmark)
+                    ForEach(allLandmarks) { currentLandmark in
+                        
+                        NavigationLink {
+                            DetailView(item: currentLandmark)
+                        } label: {
+                            GalleryItemView(item: currentLandmark)
+                        }
+                        .tint(.primary)
+                        
                     }
-                    
-                }
 
+                }
             }
             .navigationTitle("Landmarks")
                         
