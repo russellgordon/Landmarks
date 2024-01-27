37df1829a1b98ffc001acfcfacda50f882d525dc
Russell Gordon <rgordon@lcs.on.ca>
2024-01-26 22:31:34 -0500

    Applied abstraction to make detail view show properties of an instance of the Landmark structure.

--- Landmarks/Views/DetailView.swift
+++ Landmarks/Views/DetailView.swift
@@ -9,14 +9,18 @@ import SwiftUI
 
 struct DetailView: View {
     
+    // MARK: Stored properties
+    let item: Landmark
+    
+    // MARK: Computed properties
     var body: some View {
         ScrollView {
-            Image("SudburyNickel")
+            Image(item.image)
                 .resizable()
                 .scaledToFit()
             
             HStack {
-                Image(systemName: "hand.thumbsdown")
+                Image(systemName: item.isRecommended == true ? "hand.thumbsup.fill" : "hand.thumbsdown")
                     .resizable()
                     .scaledToFit()
                     .frame(height: 20)
@@ -26,19 +30,22 @@ struct DetailView: View {
             }
             .padding(.horizontal)
             
-            Text("""
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. A pellentesque sit amet porttitor eget. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Eget duis at tellus at urna condimentum. Sapien et ligula ullamcorper malesuada proin libero nunc consequat. Urna nunc id cursus metus aliquam. Eget felis eget nunc lobortis mattis aliquam. Pharetra convallis posuere morbi leo urna molestie. Lacus laoreet non curabitur gravida arcu ac tortor dignissim convallis. Sit amet mattis vulputate enim.
-
-                Sit amet facilisis magna etiam tempor orci. Quis eleifend quam adipiscing vitae proin sagittis. Mollis nunc sed id semper risus in. Pellentesque dignissim enim sit amet venenatis. Laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt eget. Eu volutpat odio facilisis mauris. Purus gravida quis blandit turpis cursus in hac habitasse platea. Nibh tortor id aliquet lectus proin nibh nisl condimentum. Fringilla ut morbi tincidunt augue interdum velit euismod. Sed ullamcorper morbi tincidunt ornare massa eget egestas.
-                """)
+            Text(item.description)
             .padding(.horizontal)
         }
-        .navigationTitle("Sudbury Nickel")
+        .navigationTitle(item.name)
     }
 }
 
 #Preview {
     NavigationStack {
-        DetailView()
+        DetailView(item: rom)
     }
 }
+
+#Preview {
+    NavigationStack {
+        DetailView(item: sudburyNickel)
+    }
+}
+
