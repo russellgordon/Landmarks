d0475b7e2830006187754094560d517d15c10851
Russell Gordon <rgordon@lcs.on.ca>
2024-01-27 09:01:14 -0500

    Almost finished adding our LazyVGrid to GalleryView.

--- Landmarks/Views/GalleryView.swift
+++ Landmarks/Views/GalleryView.swift
@@ -13,14 +13,18 @@ struct GalleryView: View {
         
         NavigationStack {
             
-            List(allLandmarks) { currentLandmark in
-                
-                NavigationLink {
-                    DetailView(item: currentLandmark)
-                } label: {
-                    GalleryItemView(item: currentLandmark)
+            LazyVGrid {
+
+                ForEach(allLandmarks) { currentLandmark in
+                    
+                    NavigationLink {
+                        DetailView(item: currentLandmark)
+                    } label: {
+                        GalleryItemView(item: currentLandmark)
+                    }
+                    
                 }
-                
+
             }
             .navigationTitle("Landmarks")
                         
