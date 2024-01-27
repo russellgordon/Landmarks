247c4c767cc79d1cca973c061d2c723d70b8355a
Russell Gordon <rgordon@lcs.on.ca>
2024-01-26 21:02:26 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Russell Gordon on 2024-01-26.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
